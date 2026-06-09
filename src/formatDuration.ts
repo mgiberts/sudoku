cec475e2ba1ffd24cc197dfb2c574e9a4f7b4086
Martins Giberts <martins@giberts-software.com>
2026-06-09 10:03:59 +0300

    feat: simpler time format when it is over one hour

--- src/formatDuration.ts
+++ src/formatDuration.ts
@@ -6,6 +6,8 @@ export const formatDuration = (seconds: number): string => {
 	const hoursString = hours > 0 ? `${String(hours).padStart(2, "0")}h ` : "";
 	const minutesString =
 		minutes > 0 ? `${String(minutes).padStart(2, "0")}m ` : "";
+	const secondsString =
+		hours < 1 ? `${String(remainingSeconds).padStart(2, "0")}s` : "";
 
-	return `${hoursString}${minutesString}${String(remainingSeconds).padStart(2, "0")}s`;
+	return `${hoursString}${minutesString}${secondsString}`;
 };
